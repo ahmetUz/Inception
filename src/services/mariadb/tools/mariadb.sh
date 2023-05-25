7061dda163c9f6879847243cfd041b8db1966f0c
ahmetuz <pro.ahmetuzn@gmail.com>
2023-05-25 08:21:48 +0200

    done

--- src/services/mariadb/tools/mariadb.sh
+++ src/services/mariadb/tools/mariadb.sh
@@ -1,25 +1,19 @@
 #!/bin/bash
 
-#démarrer MySQL
 service mysql start;
 
-#Ensuite il faut créer notre table !
-mysql -e "CREATE DATABASE IF NOT EXISTS \`${SQL_DATABASE}\`;"
+mysql -u root -p$SQL_ROOT_PASSWORD
 
-#Je vais ensuite créer un utilisateur qui pourra la manipuler.
-mysql -e "CREATE USER IF NOT EXISTS \`${SQL_USER}\`@'localhost' IDENTIFIED BY '${SQL_PASSWORD}';"
+mysql -u root -p$SQL_ROOT_PASSWORD -e "CREATE DATABASE IF NOT EXISTS \`${SQL_DATABASE}\`;"
 
-#Je donne tous les droits à cet utilisateur
-mysql -e "GRANT ALL PRIVILEGES ON \`${SQL_DATABASE}\`.* TO \`${SQL_USER}\`@'%' IDENTIFIED BY '${SQL_PASSWORD}';"
+mysql -u root -p$SQL_ROOT_PASSWORD -e "CREATE USER IF NOT EXISTS \`${SQL_USER}\`@'localhost' IDENTIFIED BY '${SQL_PASSWORD}';"
 
-#Je vais ensuite modifier mon utilisateur root 
-mysql -e "ALTER USER 'root'@'localhost' IDENTIFIED BY '${SQL_ROOT_PASSWORD}';"
+mysql -u root -p$SQL_ROOT_PASSWORD -e "GRANT ALL PRIVILEGES ON \`${SQL_DATABASE}\`.* TO \`${SQL_USER}\`@'%' IDENTIFIED BY '${SQL_PASSWORD}';"
 
-#Plus qu’à rafraichir tout cela
-mysql -e "FLUSH PRIVILEGES;"
+mysql -u root -p$SQL_ROOT_PASSWORD -e "ALTER USER 'root'@'localhost' IDENTIFIED BY '${SQL_ROOT_PASSWORD}';"
 
-#shutdown
-mysqladmin -u root -p $SQL_ROOT_PASSWORD shutdown
+mysql -u root -p$SQL_ROOT_PASSWORD -e "FLUSH PRIVILEGES;"
 
-#execute service
+mysqladmin -u root -p$SQL_ROOT_PASSWORD shutdown
+
 exec mysqld_safe
